d81d35f715f1ad5ae0ffcfcd1ea518f025764754
Manjot Singh <manjot@Manjots-MacBook-Pro.local>
2016-02-16 13:15:58 -0500

    completed for elasticsearch, reads bz2 file

--- wikipedia.org-xmldump-elasticsearch.php
+++ wikipedia.org-xmldump-elasticsearch.php
@@ -34,103 +34,90 @@ if( !$in ) {
 	abort( 'Unable to open input file.' );
 }
 
-$out = fopen( rtrim( $logpath, '/' ) . '/wikipedia.org_xmldump-' . date( 'YmdH' ) . '.log', 'w' );
-if( !$out ) {
-	abort( 'Unable to open log file.' );
-}
-
 require 'vendor/autoload.php';
 
-$hosts = [$dsname];
+$hosts = array($dsname);
 $client = \Elasticsearch\ClientBuilder::create()           // Instantiate a new ClientBuilder
                     ->setHosts($hosts)      // Set the hosts
+                    ->setRetries(6)
                     ->build();              // Build the client object
-$ds_ns = [];
 
-$time = microtime( true );
 
-$start = false;
-$chunk = null;
-$count = 0;
-$line = null;
+try {
+    $response = $client->indices()->stats();
+    print_r($response['indices']['wikipedia']['primaries']['docs']);
+} catch (Elasticsearch\Common\Exceptions\TransportException $e) {
+    $previous = $e->getPrevious();
+    print_r($previous);
+    exit;
+}
+
+print "Starting at ".date("F j, Y, g:i a");
+
+$data="";
+$line="";
+$saving=false;
+$inserted=false;
 while( !feof( $in ) ) {
 	$l = bzread( $in, 1 );
 
 	if( $l === false ) {
 		abort( 'Error reading compressed file.' );
 	}
-	if( $l == PHP_EOL ) {
-		$line = trim( $line );
-
-		if( $line == '<namespaces>' || $line == '<page>' ) {
-			$start = true;
-		}
-		if( $start === true ) {
-			$chunk .= $line . PHP_EOL;
-		}
-		if( $line == '</namespaces>' ) {
-			$start = false;
-			$chunk = str_replace( [ 'letter">', '</namespace>' ], [ 'letter" name="', '" />' ], $chunk );
-			$x = simplexml_load_string( $chunk );
-			if( $x ) {
-				foreach( $x->namespace as $y ) {
-					$y = (array)$y;
-					$dns = [ 'id' => (int)$y['@attributes']['key'], 'name' => null ];
-					if( array_key_exists( 'name', $y['@attributes'] ) ) {
-						$dns['name'] = (string)$y['@attributes']['name'];
-					}
-					$ds_ns[ $dns[ 'id' ] ] = $dns;
-				}
-				$chunk = null;
-			} else {
-				abort( 'Unable to parse namespaces.' );
-			}
-		} else if( $line == '</page>' ) {
-			$start = false;
-			$x = simplexml_load_string( $chunk );
-			$chunk = $line = null;
-			if( $x ) {
-				$dpage = [ 'index' => 'wikipedia', 'type' => 'af', 'id' => (int)$x->id, 'title' => (string)$x->title, 'ns' => $ds_ns[ (int)$x->ns ] ];
-				if( $x->redirect ) {
-					$y = (array)$x->redirect;
-					$dpage['redirect'] = $y['@attributes']['title'];
-				} else {
-					$dpage['redirect'] = false;
-				}
-				if( $x->revision ) {
-					$drev = [ 'id' => (int)$x->revision->id, 'parent' => (int)$x->revision->parentid ];
-					$drev['timestamp'] = strtotime( (string)$x->revision->timestamp ) ;
-					if( $x->revision->contributor ) {
-						$drev['contributor'] = [
-							'id' => (int)$x->revision->contributor->id,
-							'username' => (string)$x->revision->contributor->username
-						];
-					}
-					$drev['minor'] = $x->revision->minor ? true : false;
-					$drev['comment'] = (string)$x->revision->comment;
-					$drev['sha1'] = (string)$x->revision->sha1;
-					$drev['length'] = strlen( (string)$x->revision->text );
-					$drev['text'] = (string)$x->revision->text;
-					$dpage['revision'] = $drev;
-					unset( $drev );
-				}
-				$result = $client->index($dpage);
-					if( $result ) {
-						$count ++;
-						$m = date( 'Y-m-d H:i:s' ) . chr(9) . $dpage['_id'] . chr(9) . $dpage['title'] . PHP_EOL;
-						fwrite( $out, $m );
-						echo $m;
-					}
-				echo $result;
-			}
-		}
-		$line = null;
-	} else {
+	if( $l != PHP_EOL ) {
 		$line .= $l;
+	}
+	else {
+		$line = trim($line);
+
+		if($line!==false && $line != '')
+		{
+			if($line=='<page>')
+			{
+				$line .= "\n<index>wikipedia</index>\n<type>af</type><body>";
+				$saving=true;
+				$inserted=false;
+			}
+
+			if($line=='</page>')
+			{
+				$data .= "</body>\n</page>";
+				$saving=false;
+			}
+
+			if($saving)
+			{
+		  		$data.=$line;
+		  	}
+
+		  	if(!$inserted && !$saving)
+		  	{
+		  		$inserted=true;
+				//var_dump($data);
+				$xml = simplexml_load_string($data);
+				//var_dump($xml);
+				if($xml !== false)
+				{
+					try {
+						$result = $client->index($xml);
+						print 'Inserted: '.$result["_id"]."\n";
+					} catch (Elasticsearch\Common\Exceptions\TransportException $e) {
+				    	$previous = $e->getPrevious();
+	    				print_r($previous);
+					}
+				}
+
+				$data="";
+		  	}
+		}
+
+		$line = "";
 	}
 }
 
-fclose( $out );
+
 bzclose( $in );
 
-echo PHP_EOL;
+print "Finished at ".date("F j, Y, g:i a");
+
+
